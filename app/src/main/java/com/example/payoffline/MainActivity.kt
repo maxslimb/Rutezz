df2b9b6dcc9734592c008f65a64b56107365d950
maxslimb <kpatel3032@gmail.com>
2022-03-06 19:26:03 +0530

    added location permission and prompt

--- app/src/main/java/com/example/payoffline/MainActivity.kt
+++ app/src/main/java/com/example/payoffline/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.payoffline
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val wifi: WifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifi.isWifiEnabled = true
+        wifi.isWifiEnabled = false
+
         val bt_send = findViewById<Button>(R.id.button_send)
         val bt_receive = findViewById<Button>(R.id.button_receive)
         val balance = findViewById<TextView>(R.id.balance)
@@ -23,6 +44,8 @@ class MainActivity : AppCompatActivity() {
             editor.commit()
         }
         balance.text = sharedPreferences.getString("balance","0")
+        CheckPermission()
+
         bt_receive.setOnClickListener {
             val intent = Intent(this, ReceiveActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -30,10 +53,79 @@ class MainActivity : AppCompatActivity() {
         }
 
         bt_send.setOnClickListener {
-            val intent = Intent(this, SendActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
+            Onlocation()
+
         }
 
     }
+    private fun CheckPermission() {
+        Dexter.withContext(applicationContext)
+            .withPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+            .withListener(object : PermissionListener, MultiplePermissionsListener {
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+
+                }
+
+                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+
+                }
+
+            })
+            .check();
+    }
+
+    private fun Onlocation(){
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_LOW_POWER
+        }
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+        task.addOnSuccessListener {
+            val intent = Intent(this, SendActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            Log.d("MainActivity","task called")
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    val REQUEST_CHECK_SETTINGS =1
+                    exception.startResolutionForResult(this@MainActivity,
+                        REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+
+    }
+
 }
